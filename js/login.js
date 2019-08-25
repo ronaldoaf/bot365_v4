2585514a180c81a672b46447b8b40d1731b08d4d
Ronaldo Araujo de Farias <ronaldo.a.f@gmail.com>
2019-08-25 00:22:44 -0300

    bug login com credencial fixa

--- js/login.js
+++ js/login.js
@@ -6,8 +6,8 @@ function login(){
 
 		$('.hm-LoggedOutButtons_Login').click();
 		$.waitFor('.lm-StandardLogin_Username',function(){
-			$('.lm-StandardLogin_Username').val('gislene_cris');
-			$('.lm-StandardLogin_Password').val('rr842135');
+			$('.lm-StandardLogin_Username').val(localStorage['usuario_bet365']);
+			$('.lm-StandardLogin_Password').val(localStorage['senha_bet365']);
 			$('.lm-Checkbox_Input').addClass('lm-Checkbox_Input-Checked');
 			$('.lm-StandardLogin_LoginButton').click();
 			
